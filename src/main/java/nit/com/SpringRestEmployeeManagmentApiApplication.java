360a932ae9c403c5f9431dccd688588d5d4942e3
Bhagiratangi <bhagirathdangi42@gmail.com>
2022-03-08 15:21:07 +0530

    initial commit

---
+++ src/main/java/nit/com/SpringRestEmployeeManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package nit.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestEmployeeManagmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestEmployeeManagmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nit/com/SpringRestEmployeeManagmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nit.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestEmployeeManagmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
